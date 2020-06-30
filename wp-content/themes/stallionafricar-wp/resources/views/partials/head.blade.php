479c32c9ae3def65e08953207ab1ea4abe110071
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2020-06-30 14:50:03 +0100

    remove theme folder

--- wp-content/themes/stallionafricar-wp/resources/views/partials/head.blade.php
+++
@@ -1,8 +0,0 @@
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  @php wp_head() @endphp
-    <link href="<?= get_template_directory_uri() ?>/assets/owlcarousel/assets/owl.carousel.css" rel='stylesheet'/>
-  <link href="<?= get_template_directory_uri() ?>/assets/owlcarousel/assets/owl.theme.default.css" rel='stylesheet'/>
-</head>
